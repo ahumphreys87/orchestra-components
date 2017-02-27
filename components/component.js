ab99c2cc36b068b62bc691430ba623a280cda42b
Andrew Humphreys <ahumphreys87@googlemail.com>
2017-02-27 16:59:25 +0000

    Add redux bindings to components

--- components/component.js
+++ components/component.js
@@ -1,9 +1,16 @@
 // import { patch } from 'incremental-dom';
+import { bindActionCreators } from 'redux';
+import { observeStore } from '../redux/helpers';
+import { provider } from '../redux';
+
 const delegateEventSplitter = /^(\S+)\s*(.*)$/;
+const defaultMapState = () => ({});
+const defaultMapDispatch = dispatch => ({ dispatch });
 
 export class Component extends HTMLElement {
   constructor() {
     super();
+    this.connectToStore();
   }
 
   connectedCallback() {
@@ -53,6 +60,25 @@ export class Component extends HTMLElement {
     for (const element of elements) {
       element.removeEventListener(eventName, this);
     }
+  }
+
+  connectToStore() {
+    if (!provider.store) {
+      return;
+    }
+
+    const mapState = this.mapState || defaultMapState;
+    const mapDispatch = this.mapDispatch || defaultMapDispatch;
+    const actions = bindActionCreators(mapDispatch(), provider.store.dispatch);
+    const currentState = mapState(provider.store.getState());
+
+    this.state = currentState;
+    this.actions = actions;
+
+    observeStore(provider.store, currentState, mapState, (newState, oldState) => {
+      this.state = newState;
+      if (typeof this.storeUpdated === 'function') this.storeUpdated(oldState);
+    });
   }
 
   delegateEvents() {
